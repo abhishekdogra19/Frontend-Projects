533f3a4e314f7b9bec519d1aa8b78269d3d425ed
Abhishek Dogra <75717325+abhishekdogra19@users.noreply.github.com>
2023-04-12 20:45:10 +0530

    Checkboxes

--- Checkboxes/main.js
+++ Checkboxes/main.js
@@ -2,6 +2,7 @@ const checkboxes = document.querySelectorAll(`.inbox input[type="checkbox"]`);
 let lastChecked;
 
 function handleCheck(e) {
+  let inBetween = false;
   if (e.shiftKey && this.checked) {
     checkboxes.forEach((checkbox) => {
       if (checkbox === this || checkbox === lastChecked) {
